19b4d66d21a017b2a99ddd9aba35d968622d4b02
NoFishLikeIan <andreatitton96@gmail.com>
2019-08-09 19:40:10 +0200

    🥓 setting up hot reloading

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject dietary-monitor "0.1.0-SNAPSHOT"
+(defproject dietary-monitor "0.0.1"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns dietary-monitor.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                [compojure "1.1.5"]
+                [ring/ring-devel "1.18"]
+                [ring/ring-core]
+                [http-kit "2.3.0"]]
+  :repl-options {:init-ns dietary-monitor.core}
+  :main dietary-monitor.core)
--- src/dietary_monitor/core.clj
+++ src/dietary_monitor/core.clj
@@ -1,6 +1,26 @@
 (ns dietary-monitor.core)
+(:use org.httpkit.server)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+(run-server app {:port 8080})
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main [& args]
+  ;; The #' is useful when you want to hot-reload code
+  ;; You may want to take a look: https://github.com/clojure/tools.namespace
+  ;; and http://http-kit.org/migration.html#reload
+  (reset! server (run-server #'app {:port 8080})))
